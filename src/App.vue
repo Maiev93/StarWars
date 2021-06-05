9c36c24df078aa5b0f7c4bf6e90fa4c1efa49ea0
Elena Kalichkina <elena.kalichkina.rho@gmail.com>
2021-06-06 03:56:24 +0700

    init: start starwars project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app class="char">
+    <v-navigation-drawer class="char__nav" height="100px" width="300px">
+      <router-link class="char__link" to="/all">Все персонажи </router-link>
+      <router-link class="char__link" to="/favorite">Любимые </router-link>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  data: () => ({
+    //
+  }),
+};
+</script>
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+<style scoped>
+.char__nav {
+  margin: 0 auto;
+}
+.char__link {
+  text-decoration: none;
+  margin: 0 1%;
 }
 </style>
